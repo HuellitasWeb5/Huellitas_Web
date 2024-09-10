49b02200186d36577ffead7d0e0dff49494c83cc
Luis <anderson.eraso04@gmail.com>
2024-09-10 09:32:17 -0500

    Arreglos tipo donacion

--- src/java/clases/Persona.java
+++ src/java/clases/Persona.java
@@ -286,4 +286,18 @@ public class Persona {
         return persona;
     }
 
+    public static String getListaEnArreglosJS(String filtro, String orden) {
+        String lista = "[";
+        List<Persona> datos = Persona.getListaEnObjetos(filtro, orden);
+        for (int i = 0; i < datos.size(); i++) {
+            Persona persona = datos.get(i);
+            if (i > 0) {
+                lista += ", ";
+            }
+            lista += "['" + persona.getIdentificacion() + "', '" + persona.getNombre() + "','" + persona.getTelefono() + "','" + persona.getDireccion()+ "', '" + persona.getResidencia    () + "',"
+                    + "'" + persona.getEmail()+ "']";
+        }
+        lista += "]";
+        return lista;
+    }
 }
